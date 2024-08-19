2006aadf09376c34fe91b5be61b8317ae444db7e
Mafranco31 <mat.francois31@gmail.com>
2024-08-19 21:20:15 +0200

    mathis

--- test_webserv_mafranco/webserv.cpp
+++ test_webserv_mafranco/webserv.cpp
@@ -6,12 +6,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "read_file.cpp"
+#include <sys/event.h>
+#include "ft_memset.c"
+#include <fcntl.h>
+#include <sys/time.h>
+
+
+#define MAX_EVENTS 10
 
 int    clients[1024];
-fd_set      read_set, write_set, current;
 int         maxfd = 0, gid = 0;
 std::string        send_buffer = readFileToString("index.html");
-
 size_t  size_page = send_buffer.length();
 
 void    err(char  *msg)
@@ -24,50 +29,101 @@ void    err(char  *msg)
     exit(1);
 }
 
-int     main(int ac, char **av)
+int main(int	argc, char **argv)
 {
-    if (ac != 2)
+    if (argc != 2)
         err("Wrong number of arguments");
 
     struct sockaddr_in  serveraddr;
     socklen_t           len;
+	int	clientfd;
     int serverfd = socket(AF_INET, SOCK_STREAM, 0);
     if (serverfd == -1) err(NULL);
     maxfd = serverfd;
 
-    FD_ZERO(&current);
-    FD_SET(serverfd, &current);
-    bzero(clients, sizeof(clients));
-    bzero(&serveraddr, sizeof(serveraddr));
-
+	ft_memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons(atoi(av[1]));
+    serveraddr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(serverfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1 || listen(serverfd, 100) == -1)
-        err(NULL);
-    printf("Server FD: %d\n", serverfd);
-    //read_set = write_set = current;
-    while (1)
-    {
-        read_set = write_set = current;
-        if (select(maxfd + 1, &read_set, &write_set, 0, 0) == -1) continue;
-        for (int fd = 2; fd <= maxfd; fd++)
-        {
-            if (FD_ISSET(fd, &read_set))
-            {
-                if (fd == serverfd)
-                {
-                    int clientfd = accept(serverfd, (struct sockaddr *)&serveraddr, &len);
-                    if (clientfd == -1) continue;
-                    if (clientfd > maxfd) maxfd = clientfd;
-                    clients[clientfd] = gid++;
-                    FD_SET(clientfd, &current);
-                    send(clientfd, send_buffer.c_str(), size_page, 0);
+    if (bind(serverfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1){
+        close(serverfd);
+		err("bind");
+    }
+	if (listen(serverfd, 100) == -1){
+        close(serverfd);
+		err("listen");
+    }
+
+	printf("Server listening on port %s\n", argv[1]);
+	
+    struct kevent change_event, events[MAX_EVENTS];
+    int kq, nev;
+
+	kq = kqueue();
+    if (kq == -1) {
+        close(serverfd);
+		err("kqueue");
+    }
+
+	struct timespec timeout;
+    timeout.tv_sec = 5;   // Timeout in seconds
+    timeout.tv_nsec = 0;
+
+	EV_SET(&change_event, serverfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+	//EV_SET(&change_event, serverfd, EVFILT_WRITE, EV_ADD, 0, 0, NULL);//la
+    if (kevent(kq, &change_event, 1, NULL, 0, NULL) == -1) {
+        close(serverfd);
+		err("kevent");
+    }
+
+    while (1) {
+		// waiting for event
+        nev = kevent(kq, NULL, 0, events, MAX_EVENTS, &timeout);
+        if (nev == -1) {
+        	close(serverfd);
+			err("kevent");
+    	}
+
+        for (int i = 0; i < nev; i++) {
+            if (events[i].ident == serverfd) {
+                // New connection on the server socket
+                clientfd = accept(serverfd, NULL, NULL);
+                if (clientfd == -1) continue; // err message
+
+            	std::cout << "\033[1;32mNew client connected: " << clientfd << "\033[0m" << std::endl;
+
+                // Add the new client socket to kqueue for monitoring
+                EV_SET(&change_event, clientfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+                if (kevent(kq, &change_event, 1, NULL, 0, NULL) == -1) {
+                    close(clientfd);
+                    continue;
                 }
-                break;
-            }
+				send(clientfd, send_buffer.c_str(), size_page, 0);
+            } else if (events[i].filter == EVFILT_READ) {
+                // Data available to read from a client socket
+                char buffer[30000];
+                ssize_t bytes_read = read(events[i].ident, buffer, sizeof(buffer) - 1);
+
+                if (bytes_read > 0) {
+                    buffer[bytes_read] = '\0';
+                    std::cout << "Received from client " << events[i].ident << ": " << std::endl;
+                    // Echo back the message to the client
+                    std::cout << buffer << std::endl << "End of transmission." << std::endl;
+
+                } else if (bytes_read == 0) {
+                    // Client disconnected
+					std::cout << "\033[1;31mClient " << events[i].ident << " disconnected\033[0m" << std::endl;
+                    close(events[i].ident);
+                } else {
+                    close(events[i].ident);
+                }
+            } else if (events[i].filter == EVFILT_WRITE) {
+				send(events[i].ident, "YOOOO", 6, 0);
+			}
         }
     }
-    return (0);
+
+    close(serverfd);
+    return 0;
 }
